5780057156ca94411448b0d88f6ed0b1bd7186be
Kieron Browne <kieron.browne@gmail.com>
2017-10-18 10:46:03 +0200

    consolidate tests now dollar and franc are both Moneys

--- money_test.go
+++ money_test.go
@@ -7,29 +7,19 @@ import (
 )
 
 var _ = Describe("Money", func() {
-	Context("Dollar", func() {
-		It("can be multiplied by a number", func() {
-			fiveBucks := money.NewDollar(5)
-			product := fiveBucks.Times(2)
-			Expect(product).To(Equal(money.NewDollar(10)))
-		})
+	It("can be multiplied by a number", func() {
+		fiveBucks := money.NewDollar(5)
+		product := fiveBucks.Times(2)
+		Expect(product).To(Equal(money.NewDollar(10)))
 
-		It("equals another Dollar with the same amount", func() {
-			Expect(money.NewDollar(5)).To(Equal(money.NewDollar(5)))
-			Expect(money.NewDollar(5)).ToNot(Equal(money.NewDollar(6)))
-		})
+		fiveFrancs := money.NewFranc(5)
+		product = fiveFrancs.Times(2)
+		Expect(product).To(Equal(money.NewFranc(10)))
 	})
 
-	Context("Swiss Franc", func() {
-		It("can be multiplied by a number", func() {
-			fiveBucks := money.NewFranc(5)
-			product := fiveBucks.Times(2)
-			Expect(product).To(Equal(money.NewFranc(10)))
-		})
-
-		It("equals another Franc with the same amount", func() {
-			Expect(money.NewFranc(5)).To(Equal(money.NewFranc(5)))
-			Expect(money.NewFranc(5)).ToNot(Equal(money.NewFranc(6)))
-		})
+	It("equals another Dollar with the same amount", func() {
+		Expect(money.NewDollar(5)).To(Equal(money.NewDollar(5)))
+		Expect(money.NewDollar(5)).ToNot(Equal(money.NewDollar(6)))
+		Expect(money.NewDollar(5)).ToNot(Equal(money.NewFranc(5)))
 	})
 })
