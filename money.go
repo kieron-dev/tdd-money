eadd86543d1552903b8f504c408b29ff929be415
Kieron Browne <kieron.browne@gmail.com>
2017-10-18 10:18:58 +0200

    introduce Swiss Franc by duplicating Dollar

--- money.go
+++ money.go
@@ -4,10 +4,22 @@ type Dollar struct {
 	amount int
 }
 
+type Franc struct {
+	amount int
+}
+
 func NewDollar(amount int) Dollar {
 	return Dollar{amount: amount}
 }
 
 func (d Dollar) Times(multiplier int) Dollar {
 	return Dollar{amount: d.amount * multiplier}
+}
+
+func NewFranc(amount int) Franc {
+	return Franc{amount: amount}
+}
+
+func (f Franc) Times(multiplier int) Franc {
+	return Franc{amount: f.amount * multiplier}
 }
--- money_test.go
+++ money_test.go
@@ -7,14 +7,29 @@ import (
 )
 
 var _ = Describe("Money", func() {
-	It("can multiply a dollar amount by a number", func() {
-		fiveBucks := money.NewDollar(5)
-		product := fiveBucks.Times(2)
-		Expect(product).To(Equal(money.NewDollar(10)))
+	Context("Dollar", func() {
+		It("can be multiplied by a number", func() {
+			fiveBucks := money.NewDollar(5)
+			product := fiveBucks.Times(2)
+			Expect(product).To(Equal(money.NewDollar(10)))
+		})
+
+		It("equals another Dollar with the same amount", func() {
+			Expect(money.NewDollar(5)).To(Equal(money.NewDollar(5)))
+			Expect(money.NewDollar(5)).ToNot(Equal(money.NewDollar(6)))
+		})
 	})
 
-	It("equals another Dollar with the same amount", func() {
-		Expect(money.NewDollar(5)).To(Equal(money.NewDollar(5)))
-		Expect(money.NewDollar(5)).ToNot(Equal(money.NewDollar(6)))
+	Context("Swiss Franc", func() {
+		It("can be multiplied by a number", func() {
+			fiveBucks := money.NewFranc(5)
+			product := fiveBucks.Times(2)
+			Expect(product).To(Equal(money.NewFranc(10)))
+		})
+
+		It("equals another Franc with the same amount", func() {
+			Expect(money.NewFranc(5)).To(Equal(money.NewFranc(5)))
+			Expect(money.NewFranc(5)).ToNot(Equal(money.NewFranc(6)))
+		})
 	})
 })
